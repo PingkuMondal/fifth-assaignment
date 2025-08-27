5ee3aaff8690684a55aa38bf736a8c49c54e2345
Your Name <pingku010@gmail.com>
2025-08-27 16:44:05 +0600

    js call button and heart button function complete

---
+++ index.js
@@ -0,0 +1,86 @@
+function getElement(id){
+    const element=document.getElementById(id);
+    return element;
+}
+
+
+getElement('cart-box').addEventListener('click',function(e){
+    // heart button
+    if(e.target.className.includes('btn-heart')){
+
+        const navHeart=getElement('nav-heart').innerText
+        const totalHeart=Number(navHeart)+1;
+        getElement('nav-heart').innerText=totalHeart;
+
+     }
+
+     //nav copy button
+
+     if(e.target.className.includes('btn-copy')){
+          const btnCopy=e.target;
+          const titleCopy=btnCopy.parentNode.parentNode.children[1].innerText;
+          const noCopy=btnCopy.parentNode.parentNode.children[3].innerText;
+          const copyAlert=titleCopy+noCopy;
+
+
+        alert(copyAlert);
+        const navCopyBtn=getElement('nav-copy-btn').innerText;
+        const totalCopy=Number(navCopyBtn)+1;
+        getElement('nav-copy-btn').innerText=totalCopy;
+        }
+
+        //nav call button
+        if(e.target.className.includes('btn-call')){
+            const btnCall=e.target;
+          const titleCall=btnCall.parentNode.parentNode.children[1].innerText;
+          const noCall=btnCall.parentNode.parentNode.children[3].innerText;
+          const callAlert=titleCall+noCall;
+
+
+        alert(callAlert);
+          
+          const navCallBtn=Number(getElement('nav-call-btn').innerText);
+          if(navCallBtn>=20 ){
+            
+        
+        const totalCall=navCallBtn-20;
+        getElement('nav-call-btn').innerText=totalCall;
+
+        const cardContainer=getElement('card-aside-container');
+        let time=new Date();
+        // time.setHours(16,10,0);
+        let timeOnly = time.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+
+        const newCart=document.createElement('div');
+        newCart.innerHTML=`
+        <div class="grid grid-cols-3 justify-between items-center my-2 bg-gray-100/50 px-1 py-2 rounded-md">
+                        <div class="col-span-2">
+                            <h1 class="inter-font font-semibold text-sm text-gray-900 ">${titleCall}</h1>
+                            <p class="hind font-normal text-sm ">${noCall}</p>
+                        </div>
+                        <div class="hind font-normal text-sm col-span-1">
+                            <h1>${timeOnly}</h1>
+                        </div>
+        `
+        cardContainer.append(newCart);
+
+
+
+        }
+        else{
+            alert('do not have balance');
+        }
+    }
+
+
+       
+})
+
+getElement('btn-clean').addEventListener('click',function(e){
+    e.preventDefault();
+    const clear = getElement('card-aside-container');
+    clear.innerHTML='';
+
+
+})
+
